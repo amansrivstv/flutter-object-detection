4a0ac77a212494af5d3c95c7242f29de43eb499d
Aman Srivastava <amans18100@iiitnr.edu.in>
2020-07-03 01:13:16 +0530

    Comment

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+//imports
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:tflite/tflite.dart';
@@ -5,8 +6,11 @@ import 'dart:math' as math;
 import 'camera.dart';
 import 'bndbox.dart';
 
+
+//defining the model as a constant for easier understanding
 const String yolo = "Tiny YOLOv2";
 
+// getting camera steram
 class HomePage extends StatefulWidget {
   final List<CameraDescription> cameras;
 
@@ -18,16 +22,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<dynamic> _recognitions;
+  //defining variables for onject width and height
   int _imageHeight = 0;
   int _imageWidth = 0;
   String _model = "yolo";
 
+  //initializing model
   @override
   void initState() {
     super.initState();
     onSelect(yolo);
   }
-
+//loading assets: model and lables
   loadModel() async {
     String res;
     res = await Tflite.loadModel(
@@ -35,14 +41,14 @@ class _HomePageState extends State<HomePage> {
     labels: "assets/yolov2_tiny.txt",);
     print(res);
   }
-
+  
   onSelect(model) {
     setState(() {
       _model = model;
     });
     loadModel();
   }
-
+//fuction for setting variables after detection
   setRecognitions(recognitions, imageHeight, imageWidth) {
     setState(() {
       _recognitions = recognitions;
@@ -50,7 +56,7 @@ class _HomePageState extends State<HomePage> {
       _imageWidth = imageWidth;
     });
   }
-
+  
   @override
   Widget build(BuildContext context) {
     Size screen = MediaQuery.of(context).size;
@@ -62,6 +68,7 @@ class _HomePageState extends State<HomePage> {
                   _model,
                   setRecognitions,
                 ),
+                //creating bounding box around detected image
                 BndBox(
                     _recognitions == null ? [] : _recognitions,
                     math.max(_imageHeight, _imageWidth),
