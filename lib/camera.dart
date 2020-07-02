9430d356710c988fe014ab98bb226f58b90bc974
Aman Srivastava <amans18100@iiitnr.edu.in>
2020-07-03 01:19:43 +0530

    Comment

--- lib/camera.dart
+++ lib/camera.dart
@@ -1,3 +1,4 @@
+//importing packages
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:tflite/tflite.dart';
@@ -5,6 +6,7 @@ import 'dart:math' as math;
 
 typedef void Callback(List<dynamic> list, int h, int w);
 
+//setting up camera steam for detection
 class Camera extends StatefulWidget {
   final List<CameraDescription> cameras;
   final Callback setRecognitions;
@@ -15,7 +17,7 @@ class Camera extends StatefulWidget {
   @override
   _CameraState createState() => new _CameraState();
 }
-
+//initialize with isDetecting false
 class _CameraState extends State<Camera> {
   CameraController controller;
   bool isDetecting = false;
@@ -23,7 +25,7 @@ class _CameraState extends State<Camera> {
   @override
   void initState() {
     super.initState();
-
+//check stream is null or not
     if (widget.cameras == null || widget.cameras.length < 1) {
       print('No camera is found');
     } else {
@@ -36,12 +38,13 @@ class _CameraState extends State<Camera> {
           return;
         }
         setState(() {});
-
+//start detection
         controller.startImageStream((CameraImage img) {
           if (!isDetecting) {
             isDetecting = true;
 
             int startTime = new DateTime.now().millisecondsSinceEpoch;
+            //passing byte steam to detector tflite
               Tflite.detectObjectOnFrame(
                 bytesList: img.planes.map((plane) {
                   return plane.bytes;
@@ -51,14 +54,16 @@ class _CameraState extends State<Camera> {
                 imageWidth: img.width,
                 imageMean:0,
                 imageStd: 255.0,
+                //results per class allowed
                 numResultsPerClass: 1,
+                //detection threshold 
                 threshold: 0.2,
               ).then((recognitions) {
                 int endTime = new DateTime.now().millisecondsSinceEpoch;
                 print("Detection took ${endTime - startTime}");
-
+// setting variables after detection for creating bounding box
                 widget.setRecognitions(recognitions, img.height, img.width);
-
+//set detector false
                 isDetecting = false;
               });
           }
@@ -78,7 +83,7 @@ class _CameraState extends State<Camera> {
     if (controller == null || !controller.value.isInitialized) {
       return Container();
     }
-
+//checking if image bounding box is inside the screen, and if not, changing it to fit screen
     var tmp = MediaQuery.of(context).size;
     var screenH = math.max(tmp.height, tmp.width);
     var screenW = math.min(tmp.height, tmp.width);
